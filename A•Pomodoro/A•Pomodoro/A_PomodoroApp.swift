121ad2d733162f2301cf79aaf6698bbaa8b7dd36
Audun Steinholm <audun.steinholm@softwarelab.no>
2022-12-14 06:47:00 +0100

    App skeleton

---
+++ A•Pomodoro/A•Pomodoro/A_PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_PomodoroApp.swift
+//  A•Pomodoro
+//
+//  Created by Audun Steinholm on 11/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct A_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
